c57d74a4e710f1bacbd7b26676f49dab9d6274ef
buyus <jmyanezb@ing.uc3m.es>
2020-06-02 22:39:46 +0200

    renombrado:     matlab/ExperimentalResults/data/000response.csv -> matlab/ExperimentalResults/data/id000response.csv 	nuevo archivo:  matlab/ExperimentalResults/fig/avgparameterConverge.eps 	nuevo archivo:  matlab/ExperimentalResults/fig/avgpoleConverge.eps 	nuevo archivo:  matlab/ExperimentalResults/fig/avgsysBode.eps 	nuevo archivo:  matlab/ExperimentalResults/fig/avgsysTR.eps 	nuevo archivo:  matlab/ExperimentalResults/fig/avgtimeResponse.eps 	modificado:     matlab/ExperimentalResults/timeResponse.m

--- matlab/ExperimentalResults/timeResponse.m
+++ matlab/ExperimentalResults/timeResponse.m
@@ -1,4 +1,21 @@
 
+data=csvread("data/id000response.csv")
+t=data(:,1);
+incli=data(:,2);
+motor=data(:,3);
+
+
+fig=figure; hold on;grid on;
+
+plot(t,incli);
+plot(t,motor);
+
+ylabel(' Position (rad)          Inclination (deg)      ');
+xlabel('time (s)');
+legend('Neck inclination', 'Motor position','Location','best');
+saveas(fig,'fig/avgtimeResponse','epsc');
+
+
 data=csvread("data/000response.csv")
 t=data(:,1);
 incli=data(:,2);
